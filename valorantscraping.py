e5a97010360ffd3dc70c1e468842adb2431ed315
victorphams <victor.phamk@gmail.com>
2024-02-19 21:01:37 -0600

    Update valorantscraping.py
    finding player names

--- valorantscraping.py
+++ valorantscraping.py
@@ -15,9 +15,37 @@ import requests
 
 #----VALORANT STATS WEB SCRAPING TEST---------#
 
+# use next_sibling, previous_sibling to traverse content siblings of the same family tree
+# use next_siblings to give a generator object, then convert to list
+# use .contents to find the contents of a certain element/tag
+# use .descendants to show all elements/tags, need to convert to list
+    # you can also use .children
+
 url = "https://www.vlr.gg/295610/loud-vs-sentinels-champions-tour-2024-americas-kickoff-opening-b"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 
 # tbody = doc.find_all("tbody") # THIS IS TO FIND ALL THE TABLES IN THE PAGE
-print(tbody)
+
+tbody = doc.tbody
+trs = tbody.contents
+playerlinks = tbody.find_all('a')
+
+playerslist = tbody.find_all(['div'], class_="text-of")
+
+players = []
+
+for playerDiv in playerslist:
+    s = ''.join(filter(str.isalnum, playerDiv.text))
+    players.append(s)
+
+print(players)
+
+
+
+    
+
+
+
+
+        
