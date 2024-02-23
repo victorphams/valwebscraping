b775f2b1eca3882f057c9fb354260eaaad8ce87f
victorphams <victor.phamk@gmail.com>
2024-02-22 19:29:24 -0600

    Update valorantscraping.py
    Successfully scraping team names

--- valorantscraping.py
+++ valorantscraping.py
@@ -1,12 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 options = webdriver.ChromeOptions() # stops driver from closing immediately
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com/")
+driver.get("https://www.vlr.gg/295610/loud-vs-sentinels-champions-tour-2024-americas-kickoff-opening-b/?game=153737&tab=overview")
+
+try: # Implicit Wait: make the webdriver wait a couple of seconds to load the page before we continue onto the rest of the code
+    driver.implicitly_wait(1)
+
+    # Scraping Team Names
+    teamsElements = driver.find_elements(By.CLASS_NAME, value="wf-title-med")
+    teams = []
+    for team in teamsElements:
+        teams.append(team.text)
+    print(teams)
+    
+    
+
+finally:
+    driver.quit()
+
+
 
 
 
@@ -31,25 +50,25 @@ driver.get("https://www.google.com/")
 # use .descendants to show all elements/tags, need to convert to list
     # you can also use .children
 
-url = "https://www.vlr.gg/295610/loud-vs-sentinels-champions-tour-2024-americas-kickoff-opening-b"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
+# url = "https://www.vlr.gg/295610/loud-vs-sentinels-champions-tour-2024-americas-kickoff-opening-b"
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, "html.parser")
 
-# tbody = doc.find_all("tbody") # THIS IS TO FIND ALL THE TABLES IN THE PAGE
+# # tbody = doc.find_all("tbody") # THIS IS TO FIND ALL THE TABLES IN THE PAGE
 
-tbody = soup.tbody
-trs = tbody.contents
+# tbody = soup.tbody
+# trs = tbody.contents
 
-playerslist = tbody.find_all(['div'], class_="text-of")
+# playerslist = tbody.find_all(['div'], class_="text-of")
 
 
-players = []
+# players = []
 
-for playerDiv in playerslist:
-    s = ''.join(filter(str.isalnum, playerDiv.text))
-    players.append(s)
+# for playerDiv in playerslist:
+#     s = ''.join(filter(str.isalnum, playerDiv.text))
+#     players.append(s)
 
-print(players)
+# print(players)
 
 
 
